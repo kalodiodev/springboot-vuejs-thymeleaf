62f9fae4e2d2272d89ce59a4ae24b7793be89ed4
Athanasios Raptodimos <thanosra@gmail.com>
2021-02-02 00:48:59 +0200

    Initial commit

---
+++ src/main/java/eu/kalodiodev/vuejsthymeleaf/VuejsThymeleafApplication.java
@@ -0,0 +1,13 @@
+package eu.kalodiodev.vuejsthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuejsThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuejsThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/kalodiodev/vuejsthymeleaf/VuejsThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.kalodiodev.vuejsthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuejsThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
